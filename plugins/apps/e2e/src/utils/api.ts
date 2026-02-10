0b68edf84e1e8043d7fbc3eaccee50d443e9908b
Andrey Antukh <niwi@niwi.nz>
2026-02-10 19:18:03 +0100

    :construction: WIP

--- plugins/apps/e2e/src/utils/api.ts
+++ plugins/apps/e2e/src/utils/api.ts
@@ -15,6 +15,7 @@ export async function PenpotApi() {
   const resultLoginRequest = await fetch(
     `${apiUrl}/api/main/methods/login-with-password`,
     {
+      credentials: 'include',
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -23,7 +24,15 @@ export async function PenpotApi() {
     },
   );
 
+  console.log("AAAAAAAAAAAA", 1, apiUrl)
+  // console.log("AAAAAAAAAAAA", 2, resultLoginRequest);
+
+  console.dir(resultLoginRequest.headers, {depth:20});
+  console.log('Document Cookies:', window.document.cookie);
+
   const loginData = await resultLoginRequest.json();
+
+
   const authToken = resultLoginRequest.headers
     .get('set-cookie')
     ?.split(';')
